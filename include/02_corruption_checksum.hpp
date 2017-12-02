f67e82d39770ba17f4c34a5e2198d5612fc60444
ximilian <ximilian@ncelrnd0034.nce.amadeus.net>
2017-12-02 11:03:19 +0100

    Adding solution for day 2 part 1

--- include/02_corruption_checksum.hpp
+++ include/02_corruption_checksum.hpp
@@ -4,4 +4,7 @@ class CorruptionChecksum {
 
  public:
   static int check(const std::string& input);
+
+ private:
+  static int compute_single_checksum(const std::string& line);
 };
--- src/02_corruption_checksum.cpp
+++ src/02_corruption_checksum.cpp
@@ -1,8 +1,32 @@
 #include "02_corruption_checksum.hpp"
 
+#include <sstream>
+#include <climits>
 
-int CorruptionChecksum::check(const std::string& input)
-{
-    int checksum = 0;
-    return checksum;
+#include <iostream>
+
+int CorruptionChecksum::check(const std::string& input) {
+  int checksum = 0;
+  std::istringstream iss(input);
+  std::string line;
+  while (std::getline(iss, line)) {
+    checksum += compute_single_checksum(line);
+  }
+  return checksum;
+}
+
+int CorruptionChecksum::compute_single_checksum(const std::string& line) {
+  int largest  = -INT_MAX;
+  int smallest = INT_MAX;
+
+  std::istringstream iss(line);
+  int number = -1;
+  while (iss >> number) {
+    largest  = std::max(number, largest);
+    smallest = std::min(number, smallest);
+  }
+  if (number == -1) {
+    return 0;
+  }
+  return largest - smallest;
 }
