69a99060a589d7022f32ec516a9e2a7c95e6850b
ximilian <ximilian@ncelrnd0034.nce.amadeus.net>
2017-12-02 11:55:41 +0100

    Adding solution for day 2

--- include/02_corruption_checksum.hpp
+++ include/02_corruption_checksum.hpp
@@ -1,10 +1,15 @@
 #include <string>
+#include <functional>
 
 class CorruptionChecksum {
 
  public:
   static int check(const std::string& input);
 
+  static int check_division(const std::string& input);
+
  private:
+  static int check_base(const std::string& input, std::function<int(const std::string&)> method);
   static int compute_single_checksum(const std::string& line);
+  static int compute_single_checksum_division(const std::string& line);
 };
--- src/02_corruption_checksum.cpp
+++ src/02_corruption_checksum.cpp
@@ -2,15 +2,22 @@
 
 #include <sstream>
 #include <climits>
-
-#include <iostream>
+#include <vector>
 
 int CorruptionChecksum::check(const std::string& input) {
+  return check_base(input, CorruptionChecksum::compute_single_checksum);
+}
+
+int CorruptionChecksum::check_division(const std::string& input) {
+  return check_base(input, CorruptionChecksum::compute_single_checksum_division);
+}
+
+int CorruptionChecksum::check_base(const std::string& input, std::function<int(const std::string&)> method) {
   int checksum = 0;
   std::istringstream iss(input);
   std::string line;
   while (std::getline(iss, line)) {
-    checksum += compute_single_checksum(line);
+    checksum += method(line);
   }
   return checksum;
 }
@@ -29,4 +36,39 @@ int CorruptionChecksum::compute_single_checksum(const std::string& line) {
     return 0;
   }
   return largest - smallest;
+}
+
+int CorruptionChecksum::compute_single_checksum_division(const std::string& line) {
+  int result = 0;
+
+  std::vector<int> numbers;
+  std::istringstream iss(line);
+  int n1, n2;
+  while (iss >> n1) {
+    numbers.push_back(n1);
+  }
+
+  bool found  = false;
+  size_t idx1 = 0, idx2 = 1;
+  while (not found && not numbers.empty() && idx1<numbers.size()) {
+    n1 = numbers[idx1];
+    for (; idx2 < numbers.size(); ++idx2) {
+      n2    = numbers[idx2];
+      int r = n1 % n2;
+      if (r == 0) {
+        result = n1 / n2;
+        found  = true;
+        break;
+      }
+      r = n2 % n1;
+      if (r == 0) {
+        result = n2 / n1;
+        found  = true;
+        break;
+      }
+    }
+    idx1++;
+    idx2 = idx1 + 1;
+  }
+  return result;
 }
