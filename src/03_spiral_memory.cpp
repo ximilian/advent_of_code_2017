85682d35341bfb25dd1ca68fa4eaf11ea07ec6e3
Luca MARCHETTI <luca.marchetti@amadeus.com>
2017-12-21 10:47:30 +0100

    Solving first part of day 3

--- src/03_spiral_memory.cpp
+++ src/03_spiral_memory.cpp
@@ -1,9 +1,50 @@
 #include "03_spiral_memory.hpp"
+#include <iostream>
+#include <tuple>
 
 int getNthElement(int i) {
-  return 1 + i* 2 * 4;
+  if (i <= 0) {
+    return 1;
+  }
+
+  return i * 2 * 4 + getNthElement(i - 1);
 }
 
-int SpiralMemory::solve(const std::string& input) {
-  return getNthElement(std::stoi(input));
+std::tuple<int, int, int> computeBounds(int input) {
+  int upper_bound   = 0;
+  int lower_bound   = 0;
+  int current_level = 0;
+  bool found        = false;
+  while (not found) {
+    lower_bound = upper_bound;
+    upper_bound = getNthElement(current_level);
+    if (upper_bound >= input) {
+      found = true;
+      break;
+    }
+    current_level++;
+  }
+
+  return std::make_tuple(lower_bound, upper_bound, current_level);
+}
+
+int computeEdgeDistance(int lower_bound, int upper_bound, int input) {
+  if (input == 1) {
+    return 0;
+  }
+  int delta = (upper_bound - lower_bound)/4;
+  return abs(delta/2 - (input-lower_bound) % delta);
+}
+
+int SpiralMemory::solve(const std::string& sinput) {
+  int input = std::stoi(sinput);
+
+  auto bounds = computeBounds(input);
+
+  std::cout << "Upper bound for " << std::get<0>(bounds) << " < " << input << " < " << std::get<1>(bounds) << " | "
+            << std::get<2>(bounds) << "\n";
+
+  auto distance = computeEdgeDistance(std::get<0>(bounds), std::get<1>(bounds), input);
+
+  return std::get<2>(bounds) + distance;
 }
--- test/test_03_spiral_memory.cpp
+++ test/test_03_spiral_memory.cpp
@@ -3,34 +3,32 @@
 
 TEST(SpiralMemoryTest, solve_test) {
   std::cout << SpiralMemory::solve("1") << std::endl;
-  std::cout << SpiralMemory::solve("2") << std::endl;
-  std::cout << SpiralMemory::solve("3") << std::endl;
-  std::cout << SpiralMemory::solve("4") << std::endl;
-  std::cout << SpiralMemory::solve("5") << std::endl;
-  std::cout << SpiralMemory::solve("6") << std::endl;
+  std::cout << SpiralMemory::solve("12") << std::endl;
+  std::cout << SpiralMemory::solve("23") << std::endl;
+  std::cout << SpiralMemory::solve("1024") << std::endl;
+  std::cout << SpiralMemory::solve("277678") << std::endl;
   EXPECT_EQ(true, false);
 }
 
-// TEST(SpiralMemoryTest, solve_whenInputIs1_shouldBe0) {
-//   EXPECT_EQ(0, SpiralMemory::solve("1"));
-// }
+TEST(SpiralMemoryTest, solve_whenInputIs1_shouldBe0) {
+  EXPECT_EQ(0, SpiralMemory::solve("1"));
+}
 
-// TEST(SpiralMemoryTest, solve_whenInputIs12_shouldBe3) {
-//   EXPECT_EQ(3, SpiralMemory::solve("12"));
-// }
+TEST(SpiralMemoryTest, solve_whenInputIs12_shouldBe3) {
+  EXPECT_EQ(3, SpiralMemory::solve("12"));
+}
 
-// TEST(SpiralMemoryTest, solve_whenInputIs23_shouldBe2) {
-//   EXPECT_EQ(2, SpiralMemory::solve("23"));
-// }
+TEST(SpiralMemoryTest, solve_whenInputIs23_shouldBe2) {
+  EXPECT_EQ(2, SpiralMemory::solve("23"));
+}
 
-// TEST(SpiralMemoryTest, solve_whenInputIs1024_shouldBe31) {
-//   EXPECT_EQ(31, SpiralMemory::solve("1024"));
-// }
+TEST(SpiralMemoryTest, solve_whenInputIs1024_shouldBe31) {
+  EXPECT_EQ(31, SpiralMemory::solve("1024"));
+}
 
-// TEST(SpiralMemoryTest, solve_solvePuzzleInputPartOne) {
-//   EXPECT_EQ(0, SpiralMemory::solve("277678"));
-
-// }
+TEST(SpiralMemoryTest, solve_solvePuzzleInputPartOne) {
+  EXPECT_EQ(0, SpiralMemory::solve("277678"));
+}
 
 // TEST(SpiralMemoryTest, solve_solvePuzzleInputPartTwo) {
 // }
